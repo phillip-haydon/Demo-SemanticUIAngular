f81b2fe7bb7a5daa758f0f239954fe17f75ef6e1
Phillip Haydon <phillip.haydon@gmail.com>
2013-10-05 00:50:48 +0800

    adding for blog post :)

---
+++ src/DemoWebsite/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoWebsite
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
